5577cea18704f2f5be0bb659f71b04fee722ba96
muex <1096415+muex@users.noreply.github.com>
2020-12-04 23:29:26 +0100

    Added a dummy chart to default controller

--- src/Controller/DefaultController.php
+++ src/Controller/DefaultController.php
@@ -5,16 +5,33 @@ namespace App\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\UX\Chartjs\Builder\ChartBuilderInterface;
+use Symfony\UX\Chartjs\Model\Chart;
 
 class DefaultController extends AbstractController
 {
     /**
      * @Route("/", name="default")
      */
-    public function index(): Response
+    public function index(ChartBuilderInterface $chartBuilder): Response
     {
+        $chart = $chartBuilder->createChart(Chart::TYPE_LINE);
+        $chart->setData([
+            'labels' => ['Januar', 'Februar', 'März', 'April', 'Mai', 'Juni', 'Juli', 'August', 'September', 'Oktober','November', 'Dezember'],
+            'datasets' => [
+                [
+                    'label' => 'Urlaubstage',
+                    'backgroundColor' => 'rgb(255, 99, 132)',
+                    'borderColor' => 'rgb(255, 99, 132)',
+                    'data' => [2, 0, 5, 3, 4, 5, 5, 0, 0, 0, 0, 8],
+                ]
+            ]
+        ]);
+        $chart->setOptions([/* ... */]);
+
         return $this->render('default/index.html.twig', [
             'controller_name' => 'DefaultController',
+            'chart' => $chart
         ]);
     }
 }
