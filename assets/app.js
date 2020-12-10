43c4ea7e99eb72649b573cb8204ecc18c36dc1a7
muex <1096415+muex@users.noreply.github.com>
2020-12-10 22:11:55 +0100

    add bootstrap and global jquery

--- assets/app.js
+++ assets/app.js
@@ -4,9 +4,11 @@
  * We recommend including the built version of this JavaScript file
  * (and its CSS file) in your base layout (base.html.twig).
  */
+const $ = require('jquery');
+global.$ = global.jQuery = $;
 
+require('bootstrap');
 // any CSS you import will output into a single css file (app.css in this case)
 import './styles/app.scss';
-
 // start the Stimulus application
 import './bootstrap';
