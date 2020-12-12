a3554eb4d1d128d544141535647ad8ede36507a3
muex <1096415+muex@users.noreply.github.com>
2020-12-12 20:10:11 +0100

    added methods for pages

--- src/Controller/DefaultController.php
+++ src/Controller/DefaultController.php
@@ -57,4 +57,129 @@ class DefaultController extends AbstractController
             'chart' => $chart,
         ]);
     }
+
+    /**
+     * @Route("/calendar/", name="calendar")
+     */
+    public function calendar(ChartBuilderInterface $chartBuilder): Response
+    {
+        $chart = $chartBuilder->createChart(Chart::TYPE_BAR);
+        $chart->setData([
+            'labels' => ['Januar', 'Februar', 'März', 'April', 'Mai', 'Juni', 'Juli', 'August', 'September', 'Oktober','November', 'Dezember'],
+            'datasets' => [
+                [
+                    'label' => 'Urlaubstage',
+                    'backgroundColor' => 'rgb(255, 99, 132)',
+                    'borderColor' => 'rgb(255, 99, 132)',
+                    'data' => [2, 0, 5, 3, 4, 5, 5, 0, 0, 0, 0, 8],
+                ]
+            ]
+        ]);
+        $chart->setOptions([/* ... */]);
+
+        return $this->render('default/calendar.html.twig', [
+            'controller_name' => 'DefaultController',
+            'chart' => $chart,
+        ]);
+    }
+
+    /**
+     * @Route("/humanresources/", name="humanresources")
+     */
+    public function humanresources(ChartBuilderInterface $chartBuilder): Response
+    {
+        $chart = $chartBuilder->createChart(Chart::TYPE_BAR);
+        $chart->setData([
+            'labels' => ['Januar', 'Februar', 'März', 'April', 'Mai', 'Juni', 'Juli', 'August', 'September', 'Oktober','November', 'Dezember'],
+            'datasets' => [
+                [
+                    'label' => 'Urlaubstage',
+                    'backgroundColor' => 'rgb(255, 99, 132)',
+                    'borderColor' => 'rgb(255, 99, 132)',
+                    'data' => [2, 0, 5, 3, 4, 5, 5, 0, 0, 0, 0, 8],
+                ]
+            ]
+        ]);
+        $chart->setOptions([/* ... */]);
+
+        return $this->render('default/humanresources.html.twig', [
+            'controller_name' => 'DefaultController',
+            'chart' => $chart,
+        ]);
+    }
+
+    /**
+     * @Route("/management/", name="management")
+     */
+    public function management(ChartBuilderInterface $chartBuilder): Response
+    {
+        $chart = $chartBuilder->createChart(Chart::TYPE_BAR);
+        $chart->setData([
+            'labels' => ['Januar', 'Februar', 'März', 'April', 'Mai', 'Juni', 'Juli', 'August', 'September', 'Oktober','November', 'Dezember'],
+            'datasets' => [
+                [
+                    'label' => 'Urlaubstage',
+                    'backgroundColor' => 'rgb(255, 99, 132)',
+                    'borderColor' => 'rgb(255, 99, 132)',
+                    'data' => [2, 0, 5, 3, 4, 5, 5, 0, 0, 0, 0, 8],
+                ]
+            ]
+        ]);
+        $chart->setOptions([/* ... */]);
+
+        return $this->render('default/management.html.twig', [
+            'controller_name' => 'DefaultController',
+            'chart' => $chart,
+        ]);
+    }
+
+    /**
+     * @Route("/vacation/", name="vacation")
+     */
+    public function vacation(ChartBuilderInterface $chartBuilder): Response
+    {
+        $chart = $chartBuilder->createChart(Chart::TYPE_BAR);
+        $chart->setData([
+            'labels' => ['Januar', 'Februar', 'März', 'April', 'Mai', 'Juni', 'Juli', 'August', 'September', 'Oktober','November', 'Dezember'],
+            'datasets' => [
+                [
+                    'label' => 'Urlaubstage',
+                    'backgroundColor' => 'rgb(255, 99, 132)',
+                    'borderColor' => 'rgb(255, 99, 132)',
+                    'data' => [2, 0, 5, 3, 4, 5, 5, 0, 0, 0, 0, 8],
+                ]
+            ]
+        ]);
+        $chart->setOptions([/* ... */]);
+
+        return $this->render('default/vacation.html.twig', [
+            'controller_name' => 'DefaultController',
+            'chart' => $chart,
+        ]);
+    }
+
+    /**
+     * @Route("/worktime/", name="worktime")
+     */
+    public function worktime(ChartBuilderInterface $chartBuilder): Response
+    {
+        $chart = $chartBuilder->createChart(Chart::TYPE_BAR);
+        $chart->setData([
+            'labels' => ['Januar', 'Februar', 'März', 'April', 'Mai', 'Juni', 'Juli', 'August', 'September', 'Oktober','November', 'Dezember'],
+            'datasets' => [
+                [
+                    'label' => 'Urlaubstage',
+                    'backgroundColor' => 'rgb(255, 99, 132)',
+                    'borderColor' => 'rgb(255, 99, 132)',
+                    'data' => [2, 0, 5, 3, 4, 5, 5, 0, 0, 0, 0, 8],
+                ]
+            ]
+        ]);
+        $chart->setOptions([/* ... */]);
+
+        return $this->render('default/worktime.html.twig', [
+            'controller_name' => 'DefaultController',
+            'chart' => $chart,
+        ]);
+    }
 }
